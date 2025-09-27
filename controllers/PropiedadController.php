58162d62a520ed962b44e2a115dcf97f1f64c0f3
EvaAka <evamgalanr@gmail.com>
2025-09-27 19:26:56 +0200

    guardado de propiedades

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -3,13 +3,16 @@ namespace Controllers;
 use MVC\Router;
 use Model\Propiedad;
 use Model\Vendedor;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager as Image;
 
 class PropiedadController {
     public static function index(Router $router) {
 
         $propiedades = Propiedad::all();
 
-        $resultado = null;
+        //Muestra mensaje condicional
+        $resultado = $_GET['resultado'] ?? null;
 
         $router->render('propiedades/admin', [
             'propiedades' => $propiedades,
@@ -22,13 +25,47 @@ class PropiedadController {
         $propiedad = new Propiedad;
         $vendedores = Vendedor::all();
 
+        //Arreglo con mensajes de errores
+        $errores = Propiedad::getErrores();
+
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $propiedad = new Propiedad($_POST['propiedad']);
+    
+
+            //Generar un nombre único
+            $nombreImagen = md5( uniqid( rand(), true)) . ".jpg";
+
+            // Realiza un resize a la imagen con intervention
+            if($_FILES['propiedad']['tmp_name']['imagen']){
+            $manager = new Image(Driver::class);
+            $imagen = $manager->read($_FILES['propiedad']['tmp_name']['imagen'])->cover(800, 600);
+            $propiedad->setImagen($nombreImagen);
             
+            }
+
+            $errores = $propiedad->validar();
+
+            if(empty($errores)) {
+
+            /** SUBIDA DE ARCHIVOS */
+
+            if(!is_dir(CARPETA_IMAGENES)) {
+                mkdir(CARPETA_IMAGENES);
+            }
+
+            // Guarda la imagen en el servidor
+            $imagen->save(CARPETA_IMAGENES . $nombreImagen);
+
+            $propiedad->guardar();
+
+            }
+
         }
 
         $router->render('propiedades/crear', [
             'propiedad' => $propiedad,
-            'vendedores' => $vendedores
+            'vendedores' => $vendedores,
+            'errores' => $errores
         ]);
     }
 
